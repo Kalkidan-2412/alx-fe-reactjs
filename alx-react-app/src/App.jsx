567aa30aef144650bc0a59200c2cb9e1c8c53456
kalkidan Ayalew <ayalewkalkidan48@gmail.com>
2025-11-04 04:34:34 +0300

    Add WelcomeMessage component to App

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -2,12 +2,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import react from 'react';
+import WelcomeMessage from './components/welcomeMessage';
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <div>
+      <WelcomeMessage />
+    </div>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
