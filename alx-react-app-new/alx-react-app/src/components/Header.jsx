6f5fa1aac9a97afb382d5e16f38d827ee5684055
kalkidan Ayalew <ayalewkalkidan48@gmail.com>
2025-11-05 13:44:54 +0300

    Refactor Header component to include styles

--- alx-react-app-new/alx-react-app/src/components/Header.jsx
+++ alx-react-app-new/alx-react-app/src/components/Header.jsx
@@ -1,8 +1,10 @@
-<header style={{ backgroundColor: 'navy', color: 'white', textAlign: 'center' }}>
+import react from 'react';
+
+function Header() {
+   <header style={{ backgroundColor: 'navy', color: 'white', textAlign: 'center' }}>
    <h1>My Favorite Cities</h1>
  </header>
 
-function Header() {
   return (
     <header>
       <h1>My Favorite Cities</h1>
